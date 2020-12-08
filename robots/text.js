89a94342c994b14710d62ff955f19990503618ad
diego-maker <62081532+diego-maker@users.noreply.github.com>
2020-12-08 17:01:29 -0300

    att

--- robots/text.js
+++ robots/text.js
@@ -1,6 +1,29 @@
 const algorithmia = require('algorithmia');
 const apiKeyAlgorithmia = require('../credendial.json').apiKey
 const sentencaSbd = require('sbd')
+const apiWatson  = require('../cedentialWatson.json').apikey
+
+const naturalLanguage = require('watson-developer-cloud/natural-language-understanding/v1') 
+
+let nlu = new naturalLanguage({
+    iam_apikey: apiWatson,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+
+})
+
+nlu.analyze({
+    text:"hi my name is cristiano ronaldo, I like play soccer and drink beer ",
+    features:{
+        keywords:{}
+    }
+},(error, response) => {
+    if(error){
+        throw error
+    }
+    console.log(JSON.stringify(response, null, 4));
+    process.exit(0)
+})
 
 async function robot(content) {
     await encontrarContentWikipedia(content)
